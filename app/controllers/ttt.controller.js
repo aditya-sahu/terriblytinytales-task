666e27dba9f6308599e496cd9d089a565af10055
Aditya Sahu <adityasahu1511@gmail.com>
2019-12-16 21:09:42 +0530

    task development completed. Testing to be done.

--- app/controllers/ttt.controller.js
+++ app/controllers/ttt.controller.js
@@ -1,13 +1,28 @@
 function getWordCount(str) {
     str = str.replace(/\W+[ ]/g, ' ');
+    
     var words = str.split(' ');
     var ctr = words.length;
     return ctr;
 }
 
 function getFreq(str) {
+    
+    // This will contain Key as the Word and respective value as their occurrence
     var hashMap = {};
-    str = str.replace(/\W+[ ]/g, ' ');
+
+    // Consider newline, tabspaces as single whitespace
+    str = str.replace(/([\n]|[\t]|[\f]|[\v]|[\r])+/gi, ' ');
+
+    // Treat Common Punctuation as single whitespace
+    str = str.replace(/([?]|[,]|[.]|[;]|[!])+[\s]/gi, ' ');
+
+    // Get rid of quotations (only at the start or end of the word)
+    str = str.replace(/(["]|[']|[’])+[\s]/gi, ' ');
+    str = str.replace(/[\s]+(["]|[']|[’])/gi, ' ');
+
+    // Different Cases with Same spelling should be considered as one word.
+    str = str.toLowerCase();
     var words = str.split(' ');
     words.forEach(word => {
         if(!hashMap[word]) {
@@ -18,7 +33,7 @@ function getFreq(str) {
     return hashMap;
 };
 
-function getNOcurrences(N,dict) {
+function getNOccurrences(N,dict) {
     // Create items array
     var items = Object.keys(dict).map(function(key) {
         return [key, dict[key]];
@@ -34,43 +49,42 @@ function getNOcurrences(N,dict) {
     return finalArr;
 }
 
-exports.findOccurences = (req, res) => {
+exports.findOccurrences = (req, res) => {
     const request = require('request');
-    const N = req.query.userInput;
-    // Processing takes place here.
-
-    // fetch the text file from URL: http://terriblytinytales.com/test.txt
     
-    request.get('http://terriblytinytales.com/test.txt', function (error, response, body) {
+    console.log('request has been received to the controller.');
+    
+    // Get the input number
+    N = req.query.userInput;
+
+    /* Processing takes place now onwards.
+    */
+
+    // fetch the text file from URL: https://terriblytinytales.com/test.txt
+    request.get('https://terriblytinytales.com/test.txt', function (error, response, body) {
+        console.log(error);
+        
         if (!error && response.statusCode == 200) {
             
-            //const data = body;
-            const data = "This is a random generated sentence which does not seem random at all all seems all all is a random which.";
+            const data = body;
+            //const data = "This is a random generated sentence which does not seem random at all all seems all all is a random which.";
             
             // Check if words are more than the desired input. 
             var wordsCount = getWordCount(data);
             if(wordsCount < N ) {
                 console.log("Not enough words, as desired by input; Displaying maximum data.");
+                N = wordsCount;
             }
-
-            // Count occurences
+            
+            // Count occurrences
             var dict = getFreq(data);
-            // Sort the occurences
-            var finalArr = getNOcurrences(N,dict);
-            var htmlString = "<html><body>";
-            htmlString = htmlString.concat("<table border='1'><th>Word</th><th>Frequency</th>");
-            for(var i = 0; i < N ; i++) {
-                htmlString = htmlString.concat("<tr>");
-                htmlString = htmlString.concat("<td>"+finalArr[i][0]+"</td>");
-                htmlString = htmlString.concat("<td>"+finalArr[i][1]+"</td>");
-                htmlString = htmlString.concat("</tr>");
-            }
-            htmlString = htmlString.concat("</table></body></html>");
-            console.log(htmlString);
-            res.write(htmlString);
-            res.end();
-            // res.send(finalArr);
+
+            // Sort the occurrences in decreasing order and slice the array so that only N words are stored.
+            var finalArr = getNOccurrences(N,dict);
+            res.send({status:200, message: finalArr});
+        }
+        else {
+            res.send({status:400, message:'Error contacting server.'});
         }
     });
-    
 };
--- app/routes/ttt.route.js
+++ app/routes/ttt.route.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
     const router = require('../controllers/ttt.controller.js');
     // Call the method to start counting occurences.
-    app.get('/findOccurences/', router.findOccurences);
+    app.get('/findOccurrences/', router.findOccurrences);
 }
